e2c6d61b0fc48fc1b40d44d415ce5cbbefc6ba8c
Vinicius Bohrer <vbsanttos@gmail.com>
2024-04-23 21:41:39 -0300

    Create Domain Project - Add Contact entity and validations - Add DDD enum

---
+++ src/PosTech.TechChallenge.Contacts.Domain/Entities/Contact.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+
+using PosTech.TechChallenge.Contacts.Domain.Enums;
+using PosTech.TechChallenge.Contacts.Validation;
+
+namespace PosTech.TechChallenge.Contacts.Domain.Entities;
+
+public class Contact
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required DDDBrazil DDD { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string Email { get; set; }
+
+    public Contact() { }
+
+    public Contact(string name, DDDBrazil ddd, string phoneNumber, string email)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        DDD = ddd;
+        PhoneNumber = phoneNumber;
+        Email = email;
+
+        Validate();
+    }
+
+    private void Validate()
+    {
+        var validator = new ContactValidator();
+        var result = validator.Validate(this);
+        if (!result.IsValid)
+        {
+            throw new ValidationException(result.Errors);
+        }
+    }
+}
---
+++ src/PosTech.TechChallenge.Contacts.Domain/Enums/DDDBrazil.cs
@@ -0,0 +1,91 @@
+namespace PosTech.TechChallenge.Contacts.Domain.Enums;
+
+public enum DDDBrazil
+{
+    // Região Sudeste
+    DDD11 = 11, // São Paulo (SP) - Capital e Região Metropolitana
+    DDD12 = 12, // São José dos Campos (SP) e Região
+    DDD13 = 13, // Santos (SP) e Região
+    DDD14 = 14, // Bauru (SP) e Região
+    DDD15 = 15, // Sorocaba (SP) e Região
+    DDD16 = 16, // Ribeirão Preto (SP) e Região
+    DDD17 = 17, // São José do Rio Preto (SP) e Região
+    DDD18 = 18, // Presidente Prudente (SP) e Região
+    DDD19 = 19, // Campinas (SP) e Região
+
+    // Região Sul
+    DDD21 = 21, // Rio de Janeiro (RJ) - Capital e Região Metropolitana
+    DDD22 = 22, // Campos dos Goytacazes (RJ) e Região
+    DDD24 = 24, // Petrópolis (RJ) e Região
+
+    // Região Espírito Santo
+    DDD27 = 27, // Vitória (ES) e Região
+    DDD28 = 28, // Cachoeiro de Itapemirim (ES) e Região
+
+    // Região Minas Gerais
+    DDD31 = 31, // Belo Horizonte (MG) - Capital e Região Metropolitana
+    DDD32 = 32, // Juiz de Fora (MG) e Região
+    DDD33 = 33, // Governador Valadares (MG) e Região
+    DDD34 = 34, // Uberlândia (MG) e Região
+    DDD35 = 35, // Poços de Caldas (MG) e Região
+    DDD37 = 37, // Divinópolis (MG) e Região
+    DDD38 = 38, // Montes Claros (MG) e Região
+
+    // Região Centro-Oeste
+    DDD41 = 41, // Curitiba (PR) - Capital e Região Metropolitana
+    DDD42 = 42, // Ponta Grossa (PR) e Região
+    DDD43 = 43, // Londrina (PR) e Região
+    DDD44 = 44, // Maringá (PR) e Região
+    DDD45 = 45, // Foz do Iguaçu (PR) e Região
+    DDD46 = 46, // Guarapuava (PR) e Região
+    DDD47 = 47, // Joinville (SC) e Região
+    DDD48 = 48, // Florianópolis (SC) e Região
+    DDD49 = 49, // Chapecó (SC) e Região
+
+    // Região Nordeste
+    DDD51 = 51, // Porto Alegre (RS) - Capital e Região Metropolitana
+    DDD53 = 53, // Pelotas (RS) e Região
+    DDD54 = 54, // Caxias do Sul (RS) e Região
+    DDD55 = 55, // Santa Maria (RS) e Região
+
+    // Região Norte
+    DDD61 = 61, // Brasília (DF) - Distrito Federal e Região Integrada de Desenvolvimento do Distrito Federal e Entorno
+    DDD62 = 62, // Goiânia (GO) e Região Metropolitana
+    DDD63 = 63, // Palmas (TO) e Região
+    DDD64 = 64, // Rio Verde (GO) e Região
+    DDD65 = 65, // Cuiabá (MT) e Região Metropolitana
+    DDD66 = 66, // Rondonópolis (MT) e Região
+    DDD67 = 67, // Campo Grande (MS) e Região Metropolitana
+    DDD68 = 68, // Rio Branco (AC) e Região
+    DDD69 = 69, // Porto Velho (RO) e Região
+
+    // Região Nordeste
+    DDD71 = 71, // Salvador (BA) - Capital e Região Metropolitana
+    DDD73 = 73, // Ilhéus (BA) e Região
+    DDD74 = 74, // Juazeiro (BA) e Região
+    DDD75 = 75, // Feira de Santana (BA) e Região
+    DDD77 = 77, // Vitória da Conquista (BA) e Região
+    DDD79 = 79, // Aracaju (SE) e Região Metropolitana
+
+    // Região Nordeste
+    DDD81 = 81, // Recife (PE) - Capital e Região Metropolitana
+    DDD82 = 82, // Maceió (AL) e Região Metropolitana
+    DDD83 = 83, // João Pessoa (PB) e Região Metropolitana
+    DDD84 = 84, // Natal (RN) e Região Metropolitana
+    DDD85 = 85, // Fortaleza (CE) - Capital e Região Metropolitana
+    DDD86 = 86, // Teresina (PI) e Região Metropolitana
+    DDD87 = 87, // Petrolina (PE) e Região
+    DDD88 = 88, // Juazeiro do Norte (CE) e Região
+    DDD89 = 89, // Parnaíba (PI) e Região
+
+    // Região Norte
+    DDD91 = 91, // Belém (PA) - Capital e Região Metropolitana
+    DDD92 = 92, // Manaus (AM) - Capital e Região Metropolitana
+    DDD93 = 93, // Santarém (PA) e Região
+    DDD94 = 94, // Marabá (PA) e Região
+    DDD95 = 95, // Macapá (AP) e Região Metropolitana
+    DDD96 = 96, // Porto Velho (RO) e Região
+    DDD97 = 97, // Boa Vista (RR) e Região
+    DDD98 = 98, // Imperatriz (MA) e Região
+    DDD99 = 99  // São Luís (MA) - Capital e Região Metropolitana
+}
