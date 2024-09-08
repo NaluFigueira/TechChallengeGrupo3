4e6d42952d2a9de3871068099a54cc014408715d
Ana Luísa Chaves Figueira <anachfigueira@gmail.com>
2024-09-08 18:08:06 -0300

    fix: pipeline

--- users/PosTech.TechChallenge.Users.Tests/Unit/UseCases/LogInUseCaseTests.cs
+++ users/PosTech.TechChallenge.Users.Tests/Unit/UseCases/LogInUseCaseTests.cs
@@ -11,7 +11,7 @@ using PosTech.TechChallenge.Users.Application.UseCases.Authentication;
 using PosTech.TechChallenge.Users.Domain.Entities;
 using PosTech.TechChallenge.Users.Tests.Builders;
 
-namespace PosTech.TechChallenge.Users.Tests.Unit.UseCases;
+namespace PosTech.TechChallenge.Users.Tests.Unit;
 
 public class LogInUseCaseTests
 {
