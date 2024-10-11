dfc1ac69eb3dd3ecaaa7c30185ee6bb5d9d986a7
Ana Luísa Chaves Figueira <anachfigueira@gmail.com>
2024-10-11 16:07:22 -0300

    feat: creating ymls for users ms, and main scripts

---
+++ remove-cluster.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------
+#----------------------------MS Users----------------------------------
+# Remove Services
+kubectl delete services sqlserver -n tech-challenge
+kubectl delete services tech-challenge-users-api-service -n tech-challenge
+kubectl delete services tech-challenge-users-grafana-service -n tech-challenge
+
+# Apply deployments
+kubectl delete deployment tech-challenge-users-db-deployment -n tech-challenge
+kubectl delete deployment tech-challenge-users-api-deployment -n tech-challenge
+kubectl delete deployment tech-challenge-users-monitoring-deployment -n tech-challenge
+
+# Apply Persistent Volume Claims
+kubectl delete pvc tech-challenge-users-db-pvc -n tech-challenge
+kubectl delete pvc tech-challenge-users-monitoring-pvc -n tech-challenge
+
+# Apply Secrets
+kubectl delete secrets acr-secret -n tech-challenge
+
+# Apply ConfigMaps
+kubectl delete configmap prometheus-config -n tech-challenge
+kubectl delete configmap grafana-config -n tech-challenge
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
+
+kubectl delete namespace tech-challenge
---
+++ start-cluster.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+kubectl apply -f namespace.yml
+
+#----------------------------------------------------------------------
+#----------------------------MS Users----------------------------------
+
+# Apply Secrets
+kubectl apply -n tech-challenge -f ./users/k8s/secrets/acr-secret.yml 
+
+# Apply ConfigMaps
+kubectl apply -n tech-challenge -f ./users/k8s/configmaps/prometheus-configmap.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/configmaps/grafana-configmap.yml 
+
+# Apply Persistent Volume Claims
+kubectl apply -n tech-challenge -f ./users/k8s/pvcs/db-volume.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/pvcs/monitoring-volume.yml 
+
+# Apply deployments
+kubectl apply -n tech-challenge -f ./users/k8s/deployments/db-deployment.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/deployments/api-deployment.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/deployments/monitoring-deployment.yml 
+
+# Apply services
+kubectl apply -n tech-challenge -f ./users/k8s/services/db-service.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/services/api-service.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/services/grafana-service.yml 
+
+#----------------------------------------------------------------------
+#----------------------------------------------------------------------
