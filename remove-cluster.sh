12ac4583df547620e0b627dbfb918cee35fda19d
Ana Luísa Chaves Figueira <anachfigueira@gmail.com>
2024-10-18 15:31:01 -0300

    feat: migrating monitoring and grafana ymls to root k8s

--- remove-cluster.sh
+++ remove-cluster.sh
@@ -5,17 +5,17 @@
 # Remove Services
 kubectl delete services sqlserver -n tech-challenge
 kubectl delete services tech-challenge-users-api-service -n tech-challenge
-kubectl delete services tech-challenge-users-grafana-service -n tech-challenge
+kubectl delete services tech-challenge-grafana-service -n tech-challenge
 
 # Apply deployments
 kubectl delete deployment tech-challenge-users-db-deployment -n tech-challenge
 kubectl delete deployment tech-challenge-users-api-deployment -n tech-challenge
-kubectl delete deployment tech-challenge-users-monitoring-deployment -n tech-challenge
+kubectl delete deployment tech-challenge-monitoring-deployment -n tech-challenge
 kubectl delete deployment tech-challenge-rabbitmq-deployment -n tech-challenge
 
 # Apply Persistent Volume Claims
 kubectl delete pvc tech-challenge-users-db-pvc -n tech-challenge
-kubectl delete pvc tech-challenge-users-monitoring-pvc -n tech-challenge
+kubectl delete pvc tech-challenge-monitoring-pvc -n tech-challenge
 kubectl delete pvc tech-challenge-rabbitmq-pvc -n tech-challenge
 
 # Apply Secrets
--- start-cluster.sh
+++ start-cluster.sh
@@ -10,22 +10,22 @@ kubectl apply -n tech-challenge -f ./users/k8s/secrets/acr-secret.yml
 
 # Apply ConfigMaps
 kubectl apply -n tech-challenge -f ./users/k8s/configmaps/prometheus-configmap.yml 
-kubectl apply -n tech-challenge -f ./users/k8s/configmaps/grafana-configmap.yml 
+kubectl apply -n tech-challenge -f ./k8s/configmaps/grafana-configmap.yml 
 
 # Apply Persistent Volume Claims
 kubectl apply -n tech-challenge -f ./users/k8s/pvcs/db-volume.yml 
-kubectl apply -n tech-challenge -f ./users/k8s/pvcs/monitoring-volume.yml 
+kubectl apply -n tech-challenge -f ./k8s/pvcs/monitoring-volume.yml 
 kubectl apply -n tech-challenge -f ./k8s/pvcs/rabbitmq-volume.yml 
 
 # Apply services
 kubectl apply -n tech-challenge -f ./users/k8s/services/db-service.yml 
 kubectl apply -n tech-challenge -f ./users/k8s/services/api-service.yml 
-kubectl apply -n tech-challenge -f ./users/k8s/services/grafana-service.yml 
+kubectl apply -n tech-challenge -f ./k8s/services/grafana-service.yml 
 
 # Apply deployments
 kubectl apply -n tech-challenge -f ./users/k8s/deployments/db-deployment.yml 
 kubectl apply -n tech-challenge -f ./users/k8s/deployments/api-deployment.yml 
-kubectl apply -n tech-challenge -f ./users/k8s/deployments/monitoring-deployment.yml 
+kubectl apply -n tech-challenge -f ./k8s/deployments/monitoring-deployment.yml 
 kubectl apply -n tech-challenge -f ./k8s/deployments/rabbitmq-deployment.yml 
 
 #----------------------------------------------------------------------
