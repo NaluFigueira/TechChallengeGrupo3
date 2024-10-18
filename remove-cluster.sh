0e389ba3480482e0259f5e270498556d1a138dbb
Ana Luísa Chaves Figueira <anachfigueira@gmail.com>
2024-10-18 19:19:59 -0300

    feat: configuring db for contacts-command

--- remove-cluster.sh
+++ remove-cluster.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
 
-#----------------------------------------------------------------------
-#----------------------------MS Users----------------------------------
-# Remove Services
-kubectl delete services sqlserver -n tech-challenge
-kubectl delete services tech-challenge-users-api-service -n tech-challenge
-kubectl delete services tech-challenge-grafana-service -n tech-challenge
-
-# Apply deployments
-kubectl delete deployment tech-challenge-users-db-deployment -n tech-challenge
-kubectl delete deployment tech-challenge-users-api-deployment -n tech-challenge
-kubectl delete deployment tech-challenge-monitoring-deployment -n tech-challenge
-kubectl delete deployment tech-challenge-rabbitmq-deployment -n tech-challenge
-
-# Apply Persistent Volume Claims
-kubectl delete pvc tech-challenge-users-db-pvc -n tech-challenge
-kubectl delete pvc tech-challenge-monitoring-pvc -n tech-challenge
-kubectl delete pvc tech-challenge-rabbitmq-pvc -n tech-challenge
-
-# Apply Secrets
-kubectl delete secrets acr-secret -n tech-challenge
-
-# Apply ConfigMaps
-kubectl delete configmap prometheus-config -n tech-challenge
-kubectl delete configmap grafana-config -n tech-challenge
-#----------------------------------------------------------------------
-#----------------------------------------------------------------------
-
 kubectl delete namespace tech-challenge
--- start-cluster.sh
+++ start-cluster.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
-kubectl apply -f namespace.yml
+kubectl apply -f ./_k8s/namespaces/namespace.yml
 
 #----------------------------------------------------------------------
 #----------------------------MS Users----------------------------------
 
 # Apply Secrets
-kubectl apply -n tech-challenge -f ./users/k8s/secrets/acr-secret.yml 
+kubectl apply -n tech-challenge -f ./_k8s/secrets/acr-secret.yml 
 
 # Apply ConfigMaps
 kubectl apply -n tech-challenge -f ./users/k8s/configmaps/prometheus-configmap.yml 
-kubectl apply -n tech-challenge -f ./k8s/configmaps/grafana-configmap.yml 
+kubectl apply -n tech-challenge -f ./_k8s/configmaps/grafana-configmap.yml 
+
+# Apply Persistent Volume
+kubectl apply -n tech-challenge -f ./users/k8s/pvs/db-pv.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/pvs/db-pv.yml 
 
 # Apply Persistent Volume Claims
 kubectl apply -n tech-challenge -f ./users/k8s/pvcs/db-volume.yml 
-kubectl apply -n tech-challenge -f ./k8s/pvcs/monitoring-volume.yml 
-kubectl apply -n tech-challenge -f ./k8s/pvcs/rabbitmq-volume.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/pvcs/db-volume.yml 
+kubectl apply -n tech-challenge -f ./_k8s/pvcs/monitoring-volume.yml 
+kubectl apply -n tech-challenge -f ./_k8s/pvcs/rabbitmq-volume.yml 
 
 # Apply services
 kubectl apply -n tech-challenge -f ./users/k8s/services/db-service.yml 
 kubectl apply -n tech-challenge -f ./users/k8s/services/api-service.yml 
-kubectl apply -n tech-challenge -f ./k8s/services/grafana-service.yml 
+kubectl apply -n tech-challenge -f ./_k8s/services/grafana-service.yml 
 
 # Apply deployments
 kubectl apply -n tech-challenge -f ./users/k8s/deployments/db-deployment.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/deployments/db-deployment.yml 
 kubectl apply -n tech-challenge -f ./users/k8s/deployments/api-deployment.yml 
-kubectl apply -n tech-challenge -f ./k8s/deployments/monitoring-deployment.yml 
-kubectl apply -n tech-challenge -f ./k8s/deployments/rabbitmq-deployment.yml 
+kubectl apply -n tech-challenge -f ./_k8s/deployments/monitoring-deployment.yml 
+kubectl apply -n tech-challenge -f ./_k8s/deployments/rabbitmq-deployment.yml 
 
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
