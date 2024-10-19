a3b53b0e8496494ddfebd9463e7a2366d196f7aa
Ana Luísa Chaves Figueira <anachfigueira@gmail.com>
2024-10-19 00:01:21 -0300

    feat: including contacts-command deployment

--- start-cluster.sh
+++ start-cluster.sh
@@ -9,7 +9,7 @@ kubectl apply -f ./_k8s/namespaces/namespace.yml
 kubectl apply -n tech-challenge -f ./_k8s/secrets/acr-secret.yml 
 
 # Apply ConfigMaps
-kubectl apply -n tech-challenge -f ./users/k8s/configmaps/prometheus-configmap.yml 
+kubectl apply -n tech-challenge -f ./_k8s/configmaps/prometheus-configmap.yml 
 kubectl apply -n tech-challenge -f ./_k8s/configmaps/grafana-configmap.yml 
 
 # Apply Persistent Volume
@@ -17,22 +17,27 @@ kubectl apply -n tech-challenge -f ./users/k8s/pvs/db-pv.yml
 kubectl apply -n tech-challenge -f ./contacts-command/k8s/pvs/db-pv.yml 
 
 # Apply Persistent Volume Claims
-kubectl apply -n tech-challenge -f ./users/k8s/pvcs/db-volume.yml 
-kubectl apply -n tech-challenge -f ./contacts-command/k8s/pvcs/db-volume.yml 
-kubectl apply -n tech-challenge -f ./_k8s/pvcs/monitoring-volume.yml 
-kubectl apply -n tech-challenge -f ./_k8s/pvcs/rabbitmq-volume.yml 
+kubectl apply -n tech-challenge -f ./_k8s/pvcs/monitoring-pvc.yml 
+kubectl apply -n tech-challenge -f ./_k8s/pvcs/rabbitmq-pvc.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/pvcs/db-pvc.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/pvcs/db-pvc.yml 
 
 # Apply services
+kubectl apply -n tech-challenge -f ./_k8s/services/grafana-service.yml 
+kubectl apply -n tech-challenge -f ./_k8s/services/prometheus-service.yml 
+kubectl apply -n tech-challenge -f ./_k8s/services/rabbitmq-service.yml 
 kubectl apply -n tech-challenge -f ./users/k8s/services/db-service.yml 
 kubectl apply -n tech-challenge -f ./users/k8s/services/api-service.yml 
-kubectl apply -n tech-challenge -f ./_k8s/services/grafana-service.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/services/db-service.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/services/api-service.yml 
 
 # Apply deployments
-kubectl apply -n tech-challenge -f ./users/k8s/deployments/db-deployment.yml 
-kubectl apply -n tech-challenge -f ./contacts-command/k8s/deployments/db-deployment.yml 
-kubectl apply -n tech-challenge -f ./users/k8s/deployments/api-deployment.yml 
 kubectl apply -n tech-challenge -f ./_k8s/deployments/monitoring-deployment.yml 
 kubectl apply -n tech-challenge -f ./_k8s/deployments/rabbitmq-deployment.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/deployments/db-deployment.yml 
+kubectl apply -n tech-challenge -f ./users/k8s/deployments/api-deployment.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/deployments/db-deployment.yml 
+kubectl apply -n tech-challenge -f ./contacts-command/k8s/deployments/api-deployment.yml 
 
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
