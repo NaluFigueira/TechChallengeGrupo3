386fb60209dc3cd5b8a465034114575d85075972
Ana Luísa Chaves Figueira <anachfigueira@gmail.com>
2024-10-25 16:47:26 -0300

    feature: including contacts-query deployment

--- start-cluster.sh
+++ start-cluster.sh
@@ -15,12 +15,14 @@ kubectl apply -n tech-challenge -f ./_k8s/configmaps/grafana-configmap.yml
 # Apply Persistent Volume
 kubectl apply -n tech-challenge -f ./users/k8s/pvs/db-pv.yml 
 kubectl apply -n tech-challenge -f ./contacts-command/k8s/pvs/db-pv.yml 
+kubectl apply -n tech-challenge -f ./contacts-query/k8s/pvs/db-pv.yml 
 
 # Apply Persistent Volume Claims
 kubectl apply -n tech-challenge -f ./_k8s/pvcs/monitoring-pvc.yml 
 kubectl apply -n tech-challenge -f ./_k8s/pvcs/rabbitmq-pvc.yml 
 kubectl apply -n tech-challenge -f ./users/k8s/pvcs/db-pvc.yml 
 kubectl apply -n tech-challenge -f ./contacts-command/k8s/pvcs/db-pvc.yml 
+kubectl apply -n tech-challenge -f ./contacts-query/k8s/pvcs/db-pvc.yml 
 
 # Apply services
 kubectl apply -n tech-challenge -f ./_k8s/services/grafana-service.yml 
@@ -30,6 +32,8 @@ kubectl apply -n tech-challenge -f ./users/k8s/services/db-service.yml
 kubectl apply -n tech-challenge -f ./users/k8s/services/api-service.yml 
 kubectl apply -n tech-challenge -f ./contacts-command/k8s/services/db-service.yml 
 kubectl apply -n tech-challenge -f ./contacts-command/k8s/services/api-service.yml 
+kubectl apply -n tech-challenge -f ./contacts-query/k8s/services/db-service.yml 
+kubectl apply -n tech-challenge -f ./contacts-query/k8s/services/api-service.yml 
 
 # Apply deployments
 kubectl apply -n tech-challenge -f ./_k8s/deployments/monitoring-deployment.yml 
@@ -38,6 +42,8 @@ kubectl apply -n tech-challenge -f ./users/k8s/deployments/db-deployment.yml
 kubectl apply -n tech-challenge -f ./users/k8s/deployments/api-deployment.yml 
 kubectl apply -n tech-challenge -f ./contacts-command/k8s/deployments/db-deployment.yml 
 kubectl apply -n tech-challenge -f ./contacts-command/k8s/deployments/api-deployment.yml 
+kubectl apply -n tech-challenge -f ./contacts-query/k8s/deployments/db-deployment.yml 
+kubectl apply -n tech-challenge -f ./contacts-query/k8s/deployments/api-deployment.yml 
 
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
